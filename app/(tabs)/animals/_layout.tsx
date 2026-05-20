a88659123e41294f9f0a215efc0ae420edcf1033
st-i-ve <stephenechikoi@gmail.com>
2026-05-20 16:59:41 +0300

    refactor(nav): convert tab files into folders with Stack layouts

---
+++ app/(tabs)/animals/_layout.tsx
@@ -0,0 +1,6 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+export default function AnimalsStackLayout() {
+  return <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: "#FFFFFF" } }} />;
+}
